0c92f7b2d619c8a0dbd4db8d1e9d154addef851e
Alejandro <alejandrochang10@gmail.com>
2019-02-10 14:50:53 -0800

    working on specific colors

--- src/Components/Calendar/days.js
+++ src/Components/Calendar/days.js
@@ -3,17 +3,18 @@ import React from 'react';
 class Days extends React.Component {
   constructor(props) {
     super(props);
-    console.log('p', props)
   }
 
   render() {
-    let borderClass = this.props.border ? "days-border" : "days-highligthed";
-
     if (this.props.day.highlighted && this.props.day.date) { // highlighted days
       return (
-        <div onClick={this.props.selDay} className={borderClass} style={{ cursor: "pointer" }}>
+        <div
+          onClick={this.props.selDay}
+          className="days-highligthed"
+          style={{ cursor: "pointer" }}
+        >
           {this.props.day.date}
-          <div className="circle"></div>
+          <div className="circle" />
         </div>
       );
     } else if (this.props.day.date) { // regular dayss
